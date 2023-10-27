ecd6e16e5fe98bfd9c99110d2c17387fe0a74a82
ccbiri <149097766+ccbiri@users.noreply.github.com>
2023-10-27 23:54:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,21 @@ searchBox.addEventListener('input', function() {
     const searchText = searchBox.value;
 
     if (searchText != "") {
-        resultElement.innerHTML = "You typed: " + searchText;
+        resultElement.textContent = "You typed: " + searchText;
     } else {
         resultElement.textContent = "";
     }
   });
+
+  const listings = document.getElementsByClassName('listings');
+
+  function openEmailClient() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    const subject = "Contact from your website";
+    const mailtoLink = `mailto:your-email@example.com?subject=${subject}&body=Name: ${name}%0D%0AEmail: ${email}%0D%0AMessage: ${message}`;
+
+    window.location.href = mailtoLink;
+}
